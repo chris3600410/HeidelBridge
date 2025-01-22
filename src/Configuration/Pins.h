e5d3e6fdc8c587087740c52cb37d7a6f8f1eb93d
chris360#0410 <32929378+chris3600410@users.noreply.github.com>
2025-01-22 22:46:46 +0100

    Update Pins.h

--- src/Configuration/Pins.h
+++ src/Configuration/Pins.h
@@ -6,7 +6,13 @@ namespace Pins
     // ESP32 GPIO18 -> MAXRS485 RO (Receiver Output)
     // ESP32 GPIO19 -> MAXRS485 DI (Driver Input)
     // ESP32 GPIO21 -> MAXRS485 DE+RE
-    constexpr uint8_t PinRX = GPIO_NUM_18;
+    // Pin connections M5 RS485:
+    // ESP32 GPIO22 -> RX / ATOMIC RS485 Base (Receiver Output)
+    // ESP32 GPIO19 -> TX / ATOMIC RS485 Base (Driver Input)
+    // ESP32 GPIO21 -> MAXRS485 DE+RE
+
+
+    constexpr uint8_t PinRX = GPIO_NUM_22;
     constexpr uint8_t PinTX = GPIO_NUM_19;
     constexpr uint8_t PinRTS = GPIO_NUM_21;
 };
